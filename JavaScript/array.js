57c7c94373a98b31f5f073de956fe0f478a759df
ashwani1098 <vikassingh95656435@gmail.com>
2026-04-23 11:05:59 +0530

    learning js

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -26,9 +26,41 @@
 // console.log(mixedArray[4][1]);
 
 
-let newArray=[1,2,3,4,5,6];
-let cubicArray=newArray.map(num=>num*num*num);
-console.log(cubicArray);
-console.log(typeof(cubicArray));
+// let newArray=[1,2,3,4,5,6];
+// let cubicArray=newArray.map(num=>num*num*num);
+// console.log(cubicArray);
+// console.log(typeof(cubicArray));
+
+
+//normal fn
+// function multiply(a,b){
+//     return a * b;
+// }
+// console.log(multiply(5,6));
+
+// fn expression
+// let multiply=function(a,b){
+//     return a * b;
+// }
+// console.log(multiply(5,6));
+
+//arrow fn
+// const multiply=(a,b,c,d)=>a*b*c*d;
+// console.log(multiply(5,6,7,8));
+// const add=(a,b) => a+b;
+// console.log(add(5,6));
+
+// const square = x => x*x;//(x) => x*x;OK
+// console.log(square(5));
+
+// arrow fn name hello name parameters and return a greeting message sue dollar fn
+// const greet = name => `Hello, ${name}!`;
+// console.log(greet("Alice"));
+
+
+
+// arrow fn with no parameters
+const greet = () => "Hello, World!";
+console.log(greet());
 
 
